53e9af154fdb0944dcd7ad99c600f90954b9f8a0
Daniel Rocha <rochadaniel15@gmail.com>
2020-05-31 21:19:46 -0300

    Switched a file name to contain a pattern

---
+++ game/logic/mechanics/removeGameObjectsArrayById.js
@@ -0,0 +1,25 @@
+import GameObject from "../../../modules/gameObject/GameObject.js";
+
+/**
+ * 
+ * @param {Array<GameObject>} gameObject reference to the gameObject
+ * @param {number} id GameObject identification
+ */
+export default function browseTheArray(gameObject, id)
+{
+    for(var i = 0; i < [...gameObject].length; i++)
+    {
+        if(Array.isArray(gameObject[i]))
+        {
+            browseTheArray(gameObject[i], id)
+            continue
+        }
+
+        if(id === gameObject[i].Id)
+        {          
+            gameObject.splice(i, 1)
+            break
+        }
+        
+    }
+}
